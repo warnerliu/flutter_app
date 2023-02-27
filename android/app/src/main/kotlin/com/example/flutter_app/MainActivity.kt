a140d8c29c73a41ec7d9696c4c381ffbbc0168fa
warnerliu <angelstarln@126.com>
2023-02-27 18:34:15 +0800

    fix: import issue

--- android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.flutter_app
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    var methodChannel: MethodChannel? = null
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         NativeMethodForFlutter(flutterEngine.dartExecutor);
     }
